b74f2594fcb83fd1089de1289d897539673e691c
Jeremie Lagarde <jer@printstacktrace.org>
2011-03-13 22:55:22 +0000

    SONARPLUGINS-1028 : Correct license header.

--- src/main/java/org/sonar/plugins/mantis/MantisSensor.java
+++ src/main/java/org/sonar/plugins/mantis/MantisSensor.java
@@ -1,7 +1,7 @@
 /*
  * Sonar plugin for Mantis
  * Copyright (C) 2011 Jérémie Lagarde
- * mailto: jer AT printstacktrace DOR org
+ * mailto: jer AT printstacktrace DOT org
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
